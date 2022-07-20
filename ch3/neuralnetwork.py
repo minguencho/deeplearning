06e04fc4220ad940b487b1398e1234939bb54f5e
minguencho <cho000130@naver.com>
2022-07-20 16:35:11 +0900

    mnist.py 삭제

--- ch3/neuralnetwork.py
+++ ch3/neuralnetwork.py
@@ -154,3 +154,16 @@ def softmax(a):
     sum_exp_a = np.sum(exp_a) 
     y = exp_a / sum_exp_a
     return y"""
+
+    
+#손글씨 숫자 인식
+import sys, os
+sys.path.append("/home/mgcho/repos/DeepLearning/deep-learning-from-scratch")  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+from dataset.mnist import load_mnist
+
+(x_train,t_train), (x_test,t_test) = \
+    load_mnist(flatten = True, normalize = False)
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
